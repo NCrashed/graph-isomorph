eb43ae100275d527f31ea1845c24fbda47e2c9c3
NCrashed <NCrashed@gmail.com>
2014-04-15 20:48:00 +0400

    Basic project structure

---
+++ source/app.d
@@ -0,0 +1,13 @@
+/**
+*   Copyright: © 2014 Anton Gushcha
+*   License: Subject to the terms of the MIT license, as written in the included LICENSE file.
+*   Authors:  NCrashed <ncrashed@gmail.com>
+*/
+module app;
+
+import std.stdio;
+
+void main()
+{
+	writeln("Edit source/app.d to start your project.");
+}
